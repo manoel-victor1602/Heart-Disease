4df60d20c0dcf009566e7ac0a2317d118697951f
manoel-victor1602 <mvfds.snf@uea.edu.br>
2018-03-13 23:51:16 -0400

    Metricas de correlação

--- Scripts/templates.py
+++ Scripts/templates.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import cross_val_score
-from Scripts.utils import pre_processes_y
+from utils import pre_processes_y
 import itertools
 import matplotlib.pyplot as plt
 import numpy as np
